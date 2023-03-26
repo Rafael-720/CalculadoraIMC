1292ea11a32fe7dbb66f9a278a9626f110321afc
GameDev RafaelO <faelmknv@yahoo.com.br>
2023-03-25 22:12:35 -0300

    tela principal criada

--- app/src/main/java/com/rafaelo/calculadoraimc/MainActivity.kt
+++ app/src/main/java/com/rafaelo/calculadoraimc/MainActivity.kt
@@ -2,10 +2,23 @@ package com.rafaelo.calculadoraimc
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var btnCalcular: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btnCalcular = findViewById(R.id.btn_calcular)
+
+        btnCalcular.setOnClickListener {
+            //val toast: Toast = Toast.makeText(this, "botao Funcionando", Toast.LENGTH_SHORT)
+            //toast.show()
+        }
     }
 }
